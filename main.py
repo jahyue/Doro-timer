ae73304db141a8919a04f77e150f1653ae4308dc
Jahyue <jasken3568@gmail.com>
2026-07-27 23:47:33 +0300

    added settings window into classes and the ability to change the time

--- main.py
+++ main.py
@@ -5,42 +5,13 @@ from pygame import mixer
 from PIL import Image
 
 ctk.set_appearance_mode('Dark')
-WORK_LENGTH = 25 
-BREAK_LENGTH = 5 
-WORK_TIME = WORK_LENGTH * 60
-BREAK_TIME = BREAK_LENGTH * 60          
+         
 task_start_y = 94      
 task_spacing = 35       
 
  
 settings_image = ctk.CTkImage(dark_image=Image.open("settings_img.png"),size=(30,30))
 mixer.init()
-def close_settings():
-    global settings
-    settings.destroy()
-    settings = None 
-def open_settings():
-    global settings 
-    # Add a widget to the new window
-    if settings is None or not settings.winfo_exists:
-        settings = ctk.CTkToplevel(main)
-        settings.geometry('300x200')
-        settings.title("Settings")
-        focus_setting = ctk.CTkLabel(settings, text="Focus Time")
-        focus_setting.pack()
-        focus_min = ctk.CTkEntry(settings,placeholder_text=str(WORK_LENGTH))
-        focus_min.pack()
-        break_setting = ctk.CTkLabel(settings, text="Break Time")
-        break_setting.pack()
-        break_min = ctk.CTkEntry(settings,placeholder_text=str(BREAK_LENGTH))
-        break_min.pack()
-        settings.protocol("WM_DELETE_WINDOW", close_settings)
-        settings.focus()
-        settings.lift()
-    else:
-        settings.focus()
-        settings.lift()
-
 class timerLogic:
     def __init__(self,master):
         self.master = master
@@ -48,15 +19,15 @@ class timerLogic:
     def update_timer(self):
         if self.master.time_left > 0:
             self.master.time_left -= 1
+        else:
+            mixer.Sound("audio/alarm.mp3").play()
+            self.switch_timer()
 
         if self.master.running:
             self.mins = self.master.time_left // 60
             self.secs = self.master.time_left % 60
             self.master.timer_frame.timer_label.configure(text=f"{self.mins:02}:{self.secs:02}")
-        else:
-            mixer.Sound("audio/alarm.mp3").play()
-            self.switch_timer()
-        self.master.main.after(1000, self.update_timer)
+            self.master.main.after(1000, self.update_timer)
 
     def start_stop(self):
 
@@ -71,12 +42,12 @@ class timerLogic:
         self.master.running = False
 
         if self.master.on_break == False:
-            self.master.time_left = WORK_TIME
-            self.master.timer_frame.timer_label.configure(text=f"{WORK_LENGTH}:00")
+            self.master.time_left = self.master.work_length * 60
+            self.master.timer_frame.timer_label.configure(text=f"{self.master.work_length}:00")
             self.master.timer_frame.block_type.configure(text="Focus Time")
         else:
-            self.master.time_left = BREAK_TIME
-            self.master.timer_frame.timer_label.configure(text=f"{BREAK_LENGTH}:00")
+            self.master.time_left = self.master.break_length * 60
+            self.master.timer_frame.timer_label.configure(text=f"{self.master.break_length}:00")
             self.master.timer_frame.block_type.configure(text="Break Time")
 
     def skip_time(self):
@@ -84,26 +55,27 @@ class timerLogic:
         self.master.on_break = not self.master.on_break
 
         if self.master.on_break:
-            self.master.time_left = BREAK_TIME
+            self.master.time_left = self.master.break_length * 60
             self.master.timer_frame.block_type.configure(text="Break Time")
-            self.master.timer_frame.timer_label.configure(text=f"{BREAK_LENGTH}:00")
+            self.master.timer_frame.timer_label.configure(text=f"{self.master.break_length}:00")
         else:
-            self.master.time_left = WORK_TIME
+            self.master.time_left = self.master.work_length * 60
             self.master.timer_frame.block_type.configure(text="Focus Time")
-            self.master.timer_frame.timer_label.configure(text=f"{WORK_LENGTH}:00")
-
+            self.master.timer_frame.timer_label.configure(text=f"{self.master.work_length}:00")
+    
 
 class timerFrame:
     def __init__(self,master,logic):
+        self.master = master
         self.logic = logic
-        self.frame = ctk.CTkFrame(master=master,
+        self.frame = ctk.CTkFrame(master=self.master.main,
         fg_color="#d81e5b", 
         width=537, 
         height=225,
         corner_radius=15)
         self.frame.place(x=33, y=31)
         
-        self.settings_btn = ctk.CTkButton(self.frame,image=settings_image,
+        self.settings_btn = ctk.CTkButton(self.frame,image=settings_image,command=self.master.settings.open_settings,
         fg_color="#e43955",
         hover_color="#f7a292",
         text_color="#fdf0d5",text="",
@@ -144,7 +116,7 @@ class timerFrame:
 
 
         self.timer_label = ctk.CTkLabel(master=self.frame,
-        text=f"{WORK_LENGTH}:00", 
+        text=f"{self.master.work_length}:00", 
         text_color="#fdf0d5",
         fg_color="transparent",
         font=("Segoe UI",48,"bold"),
@@ -206,7 +178,8 @@ class task:
         
         self.task.destroy()
         self.del_btn.destroy()
-        self.app.task_list.remove(self) 
+        self.app.task_list.remove(self)
+        self.destroy()
 
 
 class taskframe:
@@ -250,17 +223,49 @@ class taskframe:
             self.master.task_list.append(task(self.frame1,self.new_name,y,self.master))
             
 class settingsWindow:
-    def __init__(self):
-        pass
+    def __init__(self,master):
+        self.master = master
+        self.settings = None
+    def close_settings(self):
+        self.settings.destroy()
+        self.settings = None
+    def open_settings(self):
+                # Add a widget to the new window
+        if self.settings is None or not self.settings.winfo_exists():
+            self.settings = ctk.CTkToplevel(self.master.main)
+            self.settings.geometry('300x200')
+            self.settings.title("Settings")
+            self.focus_setting = ctk.CTkLabel(self.settings, text="Focus Time")
+            self.focus_setting.pack()
+            self.focus_min = ctk.CTkEntry(self.settings,placeholder_text=str(self.master.work_length))
+            self.focus_min.pack()
+            self.break_setting = ctk.CTkLabel(self.settings, text="Break Time")
+            self.break_setting.pack()
+            self.break_min = ctk.CTkEntry(self.settings,placeholder_text=str(self.master.break_length))
+            self.break_min.pack()
+            self.save_button = ctk.CTkButton(self.settings,text="Save",command=self.save_settings)
+            self.save_button.pack()
+            self.settings.protocol("WM_DELETE_WINDOW", self.close_settings)
+            self.settings.focus()
+            self.settings.lift()
+        else:
+            self.settings.focus()
+            self.settings.lift()
+    def save_settings(self):
+        self.master.work_length = int(self.focus_min.get())
+        self.master.break_length = int(self.break_min.get())
+        self.master.logic.reset()
 class app:
     def __init__(self):
+        # Adding settings
+        self.settings = settingsWindow(self)
         # Variables
         self.task_list = []                
-        self.time_left = WORK_TIME
         self.running = False
-        self.on_break = False    
-        self.settings = None 
-
+        self.on_break = False
+        self.work_length = 25 
+        self.break_length = 5 
+        self.time_left = self.work_length * 60
         # Create main app
         self.main = ctk.CTk()
         self.main.title("Doro")
@@ -275,7 +280,7 @@ class app:
             
     def create_timer_frame(self):
         self.logic = timerLogic(self)
-        self.timer_frame = timerFrame(self.main,self.logic)
+        self.timer_frame = timerFrame(self,self.logic)
     def create_tasks_frame(self):
         self.task_frame = taskframe(self)
 root = app()
